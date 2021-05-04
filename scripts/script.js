67d76e1bc223393292b7747d515742edc3aa5f2f
AgneseBettio <bettio.agnese@gmail.com>
2021-05-05 01:49:32 +0200

    milestone2

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,18 @@ const app = new Vue({
     data: {
         usersList: usersList,
         loadedPic : 'img/avatar_io.jpg',
-        chatAttiva: false,               
+        activeChat : {},
+        
+    },
+    methods : {
+        //funzione per attivare una chat solamente
+        openChatOnClick(clickedContact){
+            this.activeChat = clickedContact      
+        },
+        //funzione da passare per stampare ora corretta
+        convertDateToTime(dateString){
+            const dateFromString = moment(dateString, "DD/MM/YYYY HH:mm:ss");
+            return dateFromString.format("HH:mm")
+        }
     }
 })
