8c87d2810ca041ff55f58ead1557edcef9d223d2
AgneseBettio <bettio.agnese@gmail.com>
2021-05-06 23:50:25 +0200

    milestone 5 finita

--- scripts/script.js
+++ scripts/script.js
@@ -30,7 +30,7 @@ const app = new Vue({
                         date: currentDate,
                         text: this.writeAMsg,
                         status: 'sent',
-                        popupActive : false
+                        popupActive: false
                     }
                 )
                 this.writeAMsg = "";
@@ -40,35 +40,41 @@ const app = new Vue({
                             date: currentDate,
                             text: "ok!",
                             status: 'received',
-                            popupActive : false
+                            popupActive: false
                         }
                     )
                 }, 1000);
 
             }
         },
-      
-
         //funzione per aprire tendina 
         showDroppedMenu(i) {
             let currentMessage = this.activeChat.messages[i];
             currentMessage.popupActive = !currentMessage.popupActive
-        },  
+        },
         //funzione per cancellare messaggio
         deleteMsg(index) {
             return this.activeChat.messages.splice(index, 1)
         },
+        lastReceivedMessage(i) {
+            const lastReceivedText = this.usersList[i].messages.filter((msg) => msg.status === 'received');
+            const lastText = lastReceivedText[lastReceivedText.length - 1].text
+            return lastText;
+        },
+        selectLastAccess(i) {
+            const lastReceivedMsg = this.usersList[i].messages.filter((msg) => msg.status === 'received');
+            const lastMsgDate = lastReceivedMsg[lastReceivedMsg.length - 1].date
+            return this.convertDateToTime(lastMsgDate)
+        }
     },
     computed: {
         //creo funzione per recuperare ultimo accesso utente  
-        selectLastAccess() {
-            console.log(this.activeChat)
-            console.log(this.activeChat.messages)
+        ActiveChatLastAccess() {
             const lastReceivedMsg = this.activeChat.messages.filter((msg) => msg.status === 'received');
             const lastMsgDate = lastReceivedMsg[lastReceivedMsg.length - 1].date
             return this.convertDateToTime(lastMsgDate)
         },
-          //funzione per filtrare i contatti
+        //funzione per filtrare i contatti
         searchAContactList() {
             return this.usersList.filter((element) => {
                 return element.name.toLowerCase().startsWith(this.searchContact.toLowerCase());
@@ -76,11 +82,6 @@ const app = new Vue({
         },
         //creo funzione per recuperare ultimo messaggio utente 
         //Questa per ultimo messagio chat attiva - devo cambiare ciclo v-for
-        // lastReceivedMessage() {
-        //     const lastReceivedText = this.activeChat.messages.filter((msg) => msg.status === 'received');
-        //     const lastText = lastReceivedText[lastReceivedText.length - 1].text
-        //     return lastText;
-        // }
     },
 
 });
